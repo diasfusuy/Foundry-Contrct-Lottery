dd79756a4608ed2864e8ccc529f5f3393a0292df
diasfusuy <diasfusuy4@gmail.com>
2026-01-07 22:56:22 -0600

    Added function to use in test

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -166,4 +166,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
     function getEntranceFee() external view returns (uint256) {
         return I_ENTERENCE_FEE;
     }
+
+    function getRaffleState() external view returns(RaffleState) {
+        return s_raffleState;
+    }
 }
